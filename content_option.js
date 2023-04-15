bc4f606ca695cf200acf3e9cbd32be054d596dab
HariG007 <hariguru0304@gmail.com>
2023-04-16 00:34:06 +0530

    msg

---
+++ content_option.js
@@ -0,0 +1,86 @@
+const logotext = "G!";
+const meta = {
+    title: "G Portfolio",
+    description: "I’m Hariharan G Ui/Ux Designer,currently working in AES Pv Ltd",
+};
+
+const introdata = {
+    title: "I’m Hariharan Guru",
+    animated: {
+        first: "I'm a Design Enthusiast",
+        second: "I'm a Ui/Ux Designer",
+        third: "I'm a Frontend-Developer",
+    },
+    description: "Design is not just what it looks like, design is how it works. --Steve Jobs",
+    your_img_url: "https://media.licdn.com/dms/image/C5603AQH7mqEWbWYKsw/profile-displayphoto-shrink_400_400/0/1640958353685?e=1686787200&v=beta&t=WU15lv8Jw7L5YZg-JLltIN4nSdWxcBlADhY6CyX0VT0",
+};
+
+const dataabout = {
+    title: "Why me?",
+    aboutme: "As a UI/UX designer, my passion lies in creating visually appealing and user-friendly interfaces that seamlessly blend form and function to provide the best possible user experience.",
+};
+
+const skills = [{
+        name: "User Interface Designer",
+        value: 90,
+    },
+    {
+        name: "Prototype",
+        value: 85,
+    },
+    {
+        name: "Figma",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Android Studio(XML)",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Designing entire mockup design and wireframes in agile methodologies.",
+    },
+    {
+        title: "Front-end Developing",
+        description: "Intuitive Responsive Front-end using react js.",
+    },
+    {
+        title: "Android studio",
+        description: " Developing the Front-end Native application using XML.",
+    },
+];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "hariguru0304@gmail.com",
+    YOUR_FONE: "+91 88388227921",
+    description: ".",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "",
+    YOUR_TEMPLATE_ID: "",
+    YOUR_USER_ID: "",
+};
+
+const socialprofils = {
+    github: "https://github.com/HariG007",
+    facebook: "https://facebook.com",
+    linkedin: "linkedin.com/in/g0304",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ pages/Folios/Folios.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Folios/Folios.css'
+
+export default function Folios() {
+  return (
+    <>
+     <img className="img1" src="https://mir-s3-cdn-cf.behance.net/projects/808/da7bc2168465215.643af0fa201a3.png"></img>
+     <img className="img1" src="https://mir-s3-cdn-cf.behance.net/projects/808/3fe251157571589.637ba5d77229f.png"></img>
+   <img className="img1"  src="https://mir-s3-cdn-cf.behance.net/projects/808/1ad3ce157571421.637ba85da9c37.png" ></img>
+   <img className="img1"  src="https://mir-s3-cdn-cf.behance.net/projects/808/c32d8d157568913.637ba4a80a299.png" ></img>
+   <img className="img1"  src="https://mir-s3-cdn-cf.behance.net/projects/808/e4e04a168464017.643aeb2dd0926.png" ></img>
+   <img className="img1"  src="https://mir-s3-cdn-cf.behance.net/projects/808/9e21e0168464161.643aebde31fa6.png" ></img>
+   <img className="img1"  src="https://mir-s3-cdn-cf.behance.net/projects/808/aabedf168464617.643aedbd053d5.png" ></img>
+   <img className="img1"  src="https://mir-s3-cdn-cf.behance.net/projects/808/b2f78c168465021.643aefe714d53.png" ></img>
+    </>
+  )
+}
